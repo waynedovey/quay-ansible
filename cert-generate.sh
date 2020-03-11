60cb5637f953f3936a1fc75da2f860d300d1a275
Wayne Dovey <wdovey@gmail.com>
2020-03-11 06:25:48 +0000

    fixup

--- cert-generate.sh
+++ cert-generate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-openssl genrsa -des3 -out CA-ocp4.lab.gsslab.pek2.redhat.com.key 2048
+openssl genrsa -des3 -out CA-ocp4.gsslab.brq.redhat.com.key 2048
 
 
-openssl req -x509 -new -nodes -key CA-ocp4.lab.gsslab.pek2.redhat.com.key -sha256 -days 1825 -out CA-ocp4.lab.gsslab.pek2.redhat.com.pem
+openssl req -x509 -new -nodes -key CA-ocp4.gsslab.brq.redhat.com.key -sha256 -days 1825 -out CA-ocp4.gsslab.brq.redhat.com.pem
 
 
 openssl genrsa -out quay.ocp4.gsslab.brq.redhat.com.key 2048
@@ -11,5 +11,4 @@ openssl genrsa -out quay.ocp4.gsslab.brq.redhat.com.key 2048
 
 openssl req -new -key quay.ocp4.gsslab.brq.redhat.com.key -out quay.ocp4.gsslab.brq.redhat.com.csr
 
-openssl x509 -req -in quay.ocp4.gsslab.brq.redhat.com.csr -CA CA-ocp4.lab.gsslab.pek2.redhat.com.pem -CAkey CA-ocp4.lab.gsslab.pek2.redhat.com.key -CAcreateserial \ 
--out quay.ocp4.gsslab.brq.redhat.com.crt -days 1825 -sha256 -extfile quay.ocp4.gsslab.brq.redhat.com.ext
+openssl x509 -req -in quay.ocp4.gsslab.brq.redhat.com.csr -CA CA-ocp4.gsslab.brq.redhat.com.pem -CAkey CA-ocp4.gsslab.brq.redhat.com.key -CAcreateserial -out quay.ocp4.gsslab.brq.redhat.com.crt -days 1825 -sha256
